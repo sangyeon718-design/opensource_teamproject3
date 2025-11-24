c05705cac8aed0fc4c98f1a5b278241ec319f734
sangyeon718-design <sangyeon718@gmail.com>
2025-11-24 23:27:35 +0900

    commit by sangyeon718

---
+++ Unit_conversion.py
@@ -0,0 +1,6 @@
+while(True):
+    unit = int(input('변환할 숫자 입력:'))
+    print('cm->m:',unit/100,"m")
+    print('m->cm:',unit*100,'cm')
+    print('g->kg:',unit/1000,'kg')
+    print('kg->g:',unit*1000, 'g')
